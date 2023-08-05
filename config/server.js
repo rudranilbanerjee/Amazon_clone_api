53ba184751326d3315424c6a5dc53c747c7748b9
rudranil-rebinInfotech <rudranil.banerjee@rebininfotech.com>
2023-08-05 15:57:59 +0530

    Start

---
+++ config/server.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env("PUBLIC_URL", "https://amazon-backend-pi01.onrender.com"),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "default-secret-here"),
+    },
+  },
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
